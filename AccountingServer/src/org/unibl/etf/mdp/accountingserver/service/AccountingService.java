b404c7863317793d93aac85ad2fa67646f659844
Pero <perogrubac99@gmail.com>
2024-11-13 15:54:57 +0100

    AccountingService  implementation

--- AccountingServer/src/org/unibl/etf/mdp/accountingserver/service/AccountingService.java
+++ AccountingServer/src/org/unibl/etf/mdp/accountingserver/service/AccountingService.java
@@ -1,10 +1,20 @@
 package org.unibl.etf.mdp.accountingserver.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.rmi.RemoteException;
+import java.time.format.DateTimeFormatter;
 
 import org.unibl.etf.mdp.library.model.Invoice;
 
+import com.google.gson.Gson;
+
 public class AccountingService implements IAccountingService {
+	Gson gson = new Gson();
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
 	public AccountingService() throws RemoteException {
 		super();
@@ -16,4 +26,23 @@ public class AccountingService implements IAccountingService {
 		return invoice.getVAT();
 	}
 
+	private void saveInvoice(Invoice invoice, String supplierName) {
+		try {
+			String date = invoice.getDate().format(formatter);
+			String fileName = date + "-" + invoice.hashCode() + ".json";
+			Path directoryPath = Paths.get("suppliers", supplierName);
+			Path filePath = directoryPath.resolve(fileName);
+
+			if (Files.notExists(directoryPath)) {
+				Files.createDirectories(directoryPath);
+			}
+
+			String jsonContent = gson.toJson(invoice);
+			Files.write(filePath, jsonContent.getBytes(), StandardOpenOption.CREATE);
+
+			System.out.println("Invoice saved as JSON at: " + filePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
